38038fc4291280df1d8675f3aee3ac0a6ec52710
Tomer Noy <tomernoy1@gmail.com>
2020-04-07 01:08:47 +0300

    first commit

---
+++ BasicTest/test.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace BasicTest
+{
+    public class Test
+    {
+        [Test]
+        public void TestNuber1()
+        {
+            Console.WriteLine("Pass");
+            Assert.IsTrue(true, "Test Passed");
+        }
+    }
+}
